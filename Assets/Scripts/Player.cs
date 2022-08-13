8c602c4f8d0904c46bad81413ab388da9a691a5e
MXNXV-ERR <MXNXV-ERR@users.noreply.github.com>
2022-08-13 21:52:46 +0530

    fixedupdate from monobehaviour

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private bool jumpKey;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            jumpKey = true;
+    }
+
+    //fixed update is called every time physics update
+    private void FixedUpdate()
+    {
+        if (jumpKey)
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKey = false;
+        }
     }
 }
