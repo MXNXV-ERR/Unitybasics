472541afb773d7ba9e2adf95144508256b68722d
MXNXV-ERR <MXNXV-ERR@users.noreply.github.com>
2022-08-13 22:12:20 +0530

    dont fly too much

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private bool jumpKey;
+    private float horizontalinput;
+    private Rigidbody rigidBodyComp;
+    private bool grounded;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBodyComp = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -16,15 +19,25 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
             jumpKey = true;
+        horizontalinput = Input.GetAxis("Horizontal");
     }
 
     //fixed update is called every time physics update
     private void FixedUpdate()
     {
+        if(!grounded)
+            return;
+
         if (jumpKey)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+           rigidBodyComp.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
             jumpKey = false;
         }
+        rigidBodyComp.velocity = new Vector3(horizontalinput, rigidBodyComp.velocity.y, 0);
     }
+
+    private void OnCollisionEnter(Collision collision)=> grounded = true;
+
+    private void OnCollisionExit(Collision collision) => grounded = false;
+    
 }
